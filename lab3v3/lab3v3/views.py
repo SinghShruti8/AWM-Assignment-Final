55d953d274935b8bacf268963233dec6ff3adacc
SinghShruti8 <71876624+SinghShruti8@users.noreply.github.com>
2022-11-09 21:48:32 +0000

    Geodjango Application

---
+++ lab3v3/lab3v3/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def signup(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+    context = {'form' :form}
+    return render(request, "registration/signup.html", context)
+
+def home(request):
+    return render(request, "home.html")
+
+
+def maps(request):
+    return render(request, "map.html")
+
+def login(request):
+    return render(request, "registration/login.html")
+
